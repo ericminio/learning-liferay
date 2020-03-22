8c97477c5cc4ce6ee309b6003e16012c54ae29b3
ericminio <eric.mignot@gmail.com>
2020-03-22 10:37:32 -0700

    show me Travis

--- wait-for-lportal.sh
+++ wait-for-lportal.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo waiting for lportal...;
+sleep 180;
+
 while ! docker-compose exec postgres psql lportal -U liferay -c "select userid, screenname, emailaddress from user_";
 do
     sleep 15;
 done;
 echo lportal database is ready;
+
+docker-compose down
